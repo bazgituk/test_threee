105af146ea0a0d2758efa51bd2ef1563bb3576f1
bazgituk <bazrbcar@yahoo.co.uk>
2015-03-05 16:32:24 +0000

    Add Test to end

--- test_threee/src/test/java/test_threee/AddingProductsToBasketTest.java
+++ test_threee/src/test/java/test_threee/AddingProductsToBasketTest.java
@@ -9,7 +9,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class AddingProductsToBasket {
+public class AddingProductsToBasketTest {
 
 	Product product_one;
 	Product product_two;
