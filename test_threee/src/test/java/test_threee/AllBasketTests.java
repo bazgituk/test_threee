f00f3c30b2c2842cec83b4b8acfbb91ad0d51a17
bazgituk <bazrbcar@yahoo.co.uk>
2015-03-05 15:40:18 +0000

    Add to pom and tests

---
+++ test_threee/src/test/java/test_threee/AllBasketTests.java
@@ -0,0 +1,11 @@
+package test_threee;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AddingProductsToBasket.class })
+public class AllBasketTests {
+
+}
